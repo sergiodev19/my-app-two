23c7cfc95093a9282ad6709f4c2b62e6a14812b1
sergiodev19 <sergio.dev19@gmail.com>
2021-10-20 02:14:07 +0400

    =

--- app.js
+++ app.js
@@ -2,14 +2,11 @@ const express = require('express');
 const app = express();
 const port = process.env.APP_PORT || 3000;
 
-console.log(process.env.PORT);
-console.log(process.env.APP_PORT);
-
 app.get('/', (req, res) => {
   res.send(`
         <h1> Express Demo App </h1>
         <h4> Message: Success </h4>
-        <p> Version 2.2 </p>
+        <p> Version 3.3 </p>
         <p style="color:red;"> ${process.env.TEST_ENV} </p>
   `);
 });
