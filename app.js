111e6ddcc8f81f4e0a8d68700447f53c5a66b6e7
sergiodev19 <sergio.dev19@gmail.com>
2021-10-20 00:41:44 +0400

    -

---
+++ app.js
@@ -0,0 +1,33 @@
+const express = require('express');
+const app = express();
+const port = process.env.PORT || 3000;
+
+app.get('/', (req, res) => {
+  res.send(`
+        <h1> Express Demo App </h1>
+        <h4> Message: Success </h4>
+        <p> Version 1.1 </p>
+        <p style="color:red;"> ${process.env.TEST_ENV} </p>
+  `);
+});
+
+app.get('/products', (req, res) => {
+  res.send([
+    {
+      productId: '101',
+      price: 100
+    },
+    {
+      productId: '102',
+      price: 250
+    },
+    {
+      productId: '103',
+      price: 310
+    },
+  ]);
+});
+
+app.listen(port, () => {
+  console.log(`Demo App is up and listening to port ${port}`);
+});
